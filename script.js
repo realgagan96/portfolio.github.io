2f0a3d10fa250b67e250ca09ad17072fc6b7e7f4
Gagan Singh <gaganxsingh007@gmail.com>
2023-06-04 12:14:47 -0400

    update

---
+++ script.js
@@ -0,0 +1,20 @@
+// //VANTA.BIRDS({
+//     el: "#vantajs",
+//     birdSize: 1.00,
+//     wingSpan: 21.00,
+//     speedLimit: 4.00,
+//     //separation: 32.00,
+//     //alignment: 33.00,
+//     //cohesion: 33.00,
+//     //quantity: 2.00
+//   });
+  VANTA.NET({
+    el: "#vantajs",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00
+  })
